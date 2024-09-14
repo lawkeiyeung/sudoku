800548f350d6390850c31fac4957df646c4795a4
lawkeiyeung <lawkeiyeung@yahoo.com>
2024-09-14 17:19:58 +0930

    clear printing

--- sudoku.py
+++ sudoku.py
@@ -1,23 +1,27 @@
 from input import input
+from colorama import Fore, Style, init
+
+init(autoreset=True)
 
 board = input()
 
-def print_sudoku_with_borders(board):
+def print_borders(board):
     for i in range(9):
-        # Print horizontal border after every 3 rows
         if i % 3 == 0 and i != 0:
-            print("-" * 21)  # 21 dashes to cover grid width (including borders)
+            print("-" * 21)  # Horizontal border
 
         row_to_print = ""
         for j in range(9):
-            # Print vertical border after every 3 columns
             if j % 3 == 0 and j != 0:
-                row_to_print += "| "
+                row_to_print += Fore.WHITE + "| "
 
-            # Print each number, using 0 as a placeholder for empty spaces
-            row_to_print += str(board[i][j]) + " "
+            # Check the value and assign color
+            number = board[i][j]
+            if number == 0:
+                row_to_print += Fore.BLACK + "0 "  # Red for empty spaces
+            else:
+                row_to_print += Fore.WHITE + str(number) + " "
 
-        # Print the constructed row
         print(row_to_print)
 
-print_sudoku_with_borders(board)
+print_borders(board)
